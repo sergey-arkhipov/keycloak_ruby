186638b08d05ece72741319f92c24763fe8a08d2
Sergey Arkhipov <Sergey-Arkhipov@yandex.ru>
2025-04-04 02:25:01 +0300

    Add README.md

--- lib/global_constants.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module GlobalConstants
-  SEARCH_FORM_PERMITED_PARAMS = [search_form: [:query, :search_by_body, :search_by_range, :column, :direction,
-                                               :search_by_subtree, :date_range_form, :user, :project,
-                                               { statuses_ids: [], classifier_ids: [] }]].freeze
-end
