8ab3ba6766067b49efc3966419a1388907ad957d
Sergey Arkhipov <Sergey-Arkhipov@yandex.ru>
2025-04-04 11:33:53 +0300

    Fix-auth-concern (#7)

--- lib/keycloak_ruby/authentication.rb
+++ lib/keycloak_ruby/authentication.rb
@@ -7,24 +7,24 @@ module KeycloakRuby
     extend ActiveSupport::Concern
 
     included do
-      before_action :keycloak_authenticate_user!
-      before_action :keycloak_set_current_user
+      before_action :authenticate_user!
+      before_action :set_current_user
     end
 
     def keycloak_jwt_service
       @keycloak_jwt_service ||= KeycloakRuby::TokenService.new(session)
     end
 
-    def keycloak_authenticate_user!
-      redirect_to login_path unless keycloak_current_user&.active?
+    def authenticate_user!
+      redirect_to login_path unless current_user&.active?
     end
 
-    def keycloak_current_user
-      @keycloak_current_user ||= keycloak_jwt_service.find_user
+    def current_user
+      @current_user ||= keycloak_jwt_service.find_user
     end
 
-    def keycloak_set_current_user
-      Current.user = keycloak_current_user
+    def set_current_user
+      Current.user = current_user
     end
   end
 end
